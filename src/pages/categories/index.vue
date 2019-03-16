4fd538d886cb73f339de81ee4acef34094703e37
any1168 <any1168@163.com>
2019-03-16 16:06:03 +0800

    修改头部信息

---
+++ src/pages/categories/index.vue
@@ -0,0 +1,20 @@
+<template>
+<view>
+  wxml 结构
+</view>
+</template>
+
+<script>
+
+
+export default {
+  data () {
+   
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/my/index.vue
@@ -0,0 +1,19 @@
+<template>
+<view>
+  wxml 结构
+</view>
+</template>
+
+<script>
+
+export default {
+  data () {
+   
+  }
+
+}
+</script>
+
+<style>
+
+</style>
